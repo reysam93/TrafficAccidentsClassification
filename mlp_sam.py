bec76d4f231daa0fce3b71cdfb78959c5299afc9
Samuel Rey <samuel.rey.escudero@example.com>
2017-04-15 21:48:42 +0200

    modifyed svm, added some predicted results looking good

--- mlp_sam.py
+++ mlp_sam.py
@@ -18,7 +18,7 @@ from sklearn.svm import SVC
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 
-from sklearn.pipeline import make_pipeline
+#from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.decomposition import FastICA
@@ -26,6 +26,13 @@ from sklearn.decomposition import FastICA
 from sklearn import datasets, metrics
 #from neupy import algorithms, environment
 
+from sklearn.kernel_approximation import RBFSampler
+from sklearn.linear_model import SGDClassifier
+
+# Needed to install  imbalanced-learn (sudo pip install sudo pip install -U imbalanced-learn)
+from imblearn.under_sampling import NearMiss
+from imblearn.pipeline import make_pipeline
+from imblearn.over_sampling import ADASYN, SMOTE
 
 # Not working!
 from sklearn.decomposition import TruncatedSVD
@@ -36,7 +43,9 @@ from sklearn.decomposition import PCA
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.feature_selection import RFE
 
-
+RANDOM_STATE = 42
+# Number of components after dimennsional reduction
+K =60
 
 def print_all_data(X, Y):  
     np_y = np.array(Y)
@@ -142,25 +151,29 @@ def analyze_select_data(data, train=True):
 """
 def mlpSimpleDiv(X, Y):
     X_train, X_val, Y_train, Y_val = train_test_split(X, Y, train_size=0.8)
+    X_red_train = FastICA(n_components=K, whiten=True).fit_transform(X_train, Y)
+    X_red_val = FastICA(n_components=K, whiten=True).fit_transform(X_val, Y)
     
     # Normalization
     scalar = StandardScaler()
-    scalar.fit(X_train)
-    X_train_n = scalar.transform(X_train)
-    X_val_n = scalar.transform(X_val)
+    X_train_n = scalar.fit_transform(X_train)
+    X_val_n = scalar.fit_transform(X_val)
     
     # MLP creation + trainning
     scalar = StandardScaler()
-    mlp = MLPClassifier(activation="identity", verbose=True,
-                        max_iter=150, hidden_layer_sizes=50, early_stopping=True, 
-                        tol=1e-12, validation_fraction=0.3, alpha=1e-8, 
-                        learning_rate_init=0.0001, beta_1=0.5, warm_start=True) 
+    mlp = MLPClassifier(activation="identity", verbose=False, solver="adam",
+                        max_iter=150, hidden_layer_sizes=(100,3), early_stopping=True, 
+                        tol=1e-12, validation_fraction=0.3, alpha=1e-8,
+                        learning_rate_init=0.1, beta_1=0.5, warm_start=True,
+                        random_state=RANDOM_STATE)
+                        
+    undersample = SMOTE()
+    classifier = make_pipeline(undersample, mlp)
     # Prediction and evaluation
-    mlp.fit(X_train_n, Y_train)
-    prediction = mlp.predict(X_val_n)
-    score = mlp.score(X_val_n, Y_val)
-    print "Score:", score
-    return mlp, scalar
+    classifier.fit(X_train_n, Y_train)
+    prediction = classifier.predict(X_val_n)
+    print ("\n", classification_report(prediction, Y_val))
+    return classifier, scalar
 
 
 """
@@ -174,50 +187,106 @@ def mlpSimpleDiv(X, Y):
                         learning_rate_init=0.0001, beta_1=0.8, warm_start=True) 
 """
 def mlpCrossVal(X, Y):
-    X_red = FastICA(n_components=50, whiten=True).fit_transform(X,Y) #65
+    X_red = FastICA(n_components=K, whiten=True).fit_transform(X,Y) #65
     #X_red = NMF(n_components=30).fit_transform(X, Y)
     #X_red = TruncatedSVD(n_components=55).fit_transform(X, Y)          #65 +- 4
     #X_red = SelectKBest(mutual_info_classif, k=30).fit_transform(X, Y)
 
     
+    
     scalar = StandardScaler()
-    mlp = MLPClassifier(activation="identity", verbose=True,
-                        max_iter=150, hidden_layer_sizes=50, early_stopping=True, 
-                        tol=1e-12, validation_fraction=0.3, alpha=1e-8, 
-                        learning_rate_init=0.0001, beta_1=0.5, warm_start=True)    
-    pipeline = make_pipeline(scalar, mlp)
-    scores = cross_val_score(pipeline, X, Y, cv=10)
+    #undersample = NearMiss(version=3, random_state=RANDOM_STATE)
+    undersample = SMOTE()
+    
+    mlp = MLPClassifier(activation="identity", verbose=False, solver="adam",
+                        max_iter=150, hidden_layer_sizes=(400,3), early_stopping=True, 
+                        tol=1e-12, validation_fraction=0.3, alpha=1e-8,
+                        learning_rate_init=0.1, beta_1=0.5, warm_start=True,
+                        random_state=RANDOM_STATE)    
+    classifier = make_pipeline(undersample, mlp)
+    pipeline = make_pipeline(scalar, classifier)
+    scores = cross_val_score(pipeline, X_red, Y, cv=3)
 
     print(scores)
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-    return mlp, scalar
+    return classifier, scalar
+    
+
+# Mejor precison media:     0.80      0.62      0.66
+# Con pesos balanceados:    0.76      0.62      0.66
+def svmSimpleVal(X,Y):
+    X_train, X_val, Y_train, Y_val = train_test_split(X, Y, train_size=0.8)
+   # X_red_train = FastICA(n_components=K, whiten=True).fit_transform(X_train, Y)
+    #X_red_val = FastICA(n_components=K, whiten=True).fit_transform(X_val)
+    
+    # Normalization
+    scalar = StandardScaler()
+    X_train_n = scalar.fit_transform(X_train)
+    X_val_n = scalar.fit_transform(X_val)
+    
+    undersample = SMOTE()
+    svm = SVC(verbose=True, kernel="rbf", decision_function_shape="ovr",
+              random_state=RANDOM_STATE, C=0.008, degree=2)
+              
+    classifier = make_pipeline(undersample, svm)
+    
+    # Prediction and evaluation
+    classifier.fit(X_train_n, Y_train)
+    prediction = classifier.predict(X_val_n)
+    print ("\n", classification_report(prediction, Y_val))
+    return classifier, scalar
 
 
 def svmCrossVal(X, Y):
-    X_red = FastICA(n_components=30, whiten=True).fit_transform(X, Y)
+    X_red = FastICA(n_components=K, whiten=True).fit_transform(X, Y)
     
     scalar = StandardScaler()
-    scalar.fit_transform(X)
+    undersample = SMOTE()
     svm = SVC(verbose=True, kernel="rbf", decision_function_shape="ovr",
-              )
-    pipeline = make_pipeline(scalar, svm)
-    scores = cross_val_score(pipeline, X_red, Y, cv=8)
+              random_state=RANDOM_STATE, C=0.008, degree=2,
+              class_weight="balanced")
+    classifier = make_pipeline(undersample, svm)
+    pipeline = make_pipeline(scalar, classifier)
+    scores = cross_val_score(pipeline, X, Y, cv=5)
 
     print(scores)
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-    return scores, svm
+    return svm, scalar
 
 
 def kNeighborsCrossVal(X, Y):    
     scalar = StandardScaler()
-    kn = KNeighborsClassifier(n_neighbors=200, p=2, weights="distance")
+    kn = KNeighborsClassifier(n_neighbors=101, p=2, weights="distance")
     pipeline = make_pipeline(scalar, kn)
     scores = cross_val_score(pipeline, X, Y, cv=5)
 
     print(scores)
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-    return scores, kn
+    return kn, scalar
     
+
+def kNeighborsSimpleVal(X,Y):
+    X_train, X_val, Y_train, Y_val = train_test_split(X, Y, train_size=0.8)
+   # X_red_train = FastICA(n_components=K, whiten=True).fit_transform(X_train, Y)
+    #X_red_val = FastICA(n_components=K, whiten=True).fit_transform(X_val)
+    
+    # Normalization
+    scalar = StandardScaler()
+    X_train_n = scalar.fit_transform(X_train)
+    X_val_n = scalar.fit_transform(X_val)
+    
+    undersample = SMOTE()
+    kn = KNeighborsClassifier(n_neighbors=3001, p=2, weights="uniform",
+                              algorithm="auto")
+              
+    classifier = make_pipeline(undersample, kn)
+    
+    # Prediction and evaluation
+    classifier.fit(X_train_n, Y_train)
+    prediction = classifier.predict(X_val_n)
+    print ("\n", classification_report(prediction, Y_val))
+    return classifier, scalar
+
 
 def pnnTrainTestNoNorm(X, Y):
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.7)
@@ -267,17 +336,17 @@ def logisticRegr(X,Y):
     and predict the results.
 """
 def predictTestData(X_train, Y_train, classifier, scalar):
-    #X_red_train = FastICA(n_components=50, whiten=True).fit_transform(X_train)
+    #X_red_train = FastICA(n_components=K, whiten=True).fit_transform(X_train)
     Xn = scalar.fit_transform(X_train)
     classifier.fit(Xn, Y_train)
-        
     test_data = pd.read_csv('testdata.csv')
     X_test, Y_test = analyze_select_data(test_data, False)
-   # X_red_test = FastICA(n_components=50, whiten=True).fit_transform(X_test)
+    #X_red_test = FastICA(n_components=K, whiten=True).fit_transform(X_test)
     Xn_test = scalar.fit_transform(X_test)
-    pred = pd.DataFrame(classifier.predict(Xn_test), index=np.arange(1,201))
+    pred = pd.DataFrame(classifier.predict(Xn_test),index=np.arange(1,201))#index=np.arange(1,201)
     pred.to_csv("predicted.csv", header=["Prediction"], index_label="Id")
     #print (classifier.score(Xn_test, Y_test))
+    print ("DONE")
 
 
 """
@@ -293,11 +362,13 @@ if __name__ == "__main__":
     X, Y = analyze_select_data(train_data)
     print("Shape of X:", X.shape)
     
-    classifier, scalar = mlpSimpleDiv(X, Y)
+    #classifier, scalar = mlpSimpleDiv(X, Y)
     #classifier, scalar = mlpCrossVal(X, Y)
     #logisticRegr(X,Y)
-    #svmCrossVal(X,Y)
-    #kNe#ighborsCrossVal(X,Y)
+    #classifier, scalar = svmCrossVal(X,Y)
+    classifier, scalar = svmSimpleVal(X,Y)
+    #classifier, scalar = kNeighborsCrossVal(X,Y)
+    #clasifier, scalar = kNeighborsSimpleVal(X,Y)
     #pnnTrainTestNoNorm(X, Y)
     #pnnTrainTestNorm(X, Y)
     
